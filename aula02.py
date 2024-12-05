ea58b7e8ac0b849324f752dd5123958f26242c73
Gabesantosneves <gabis.santosneves@gmail.com>
2024-12-05 09:47:10 -0300

    Aula 2

---
+++ aula02.py
@@ -0,0 +1,9 @@
+print ("Bom diaa")
+nome_usuario = "Gabriele Neves" # tipo str = string
+idade_usuario = 19 # tipo -> int = integer
+altura_usuario = 1.65 # tipo ->  float = ponto flutuante 
+maior_idade = True # tipo -> bool + bolean
+print ('Seja Bem vindo', nome_usuario)
+print ('sua idade é', idade_usuario, 'anos')
+print ('voce tem', altura_usuario, 'metros')
+print ('voce é maior de idade?', maior_idade)
